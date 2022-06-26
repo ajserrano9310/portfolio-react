ff4644e74c365ffffdd639364c9d9259f9f1679b
alejandroserrano <ajserrano9310@gmail.com>
2022-06-26 08:09:55 -0600

    Making progress with tutorial

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
@@ -19,8 +18,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
         <Router>
           <div>
             <NavigationContainer />
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 export default function (props) {
   const { id, description, thumb_image_url, logo } = props.item;
   return (
-    <div>
-    <img src={thumb_image_url}/>
-    <img src={logo}/>
+    <div className="portfolio-item-wrapper">
+      <div className="portfolio-img-background" 
+      style={{backgroundImage: "url(" + thumb_image_url + ")"}}/>
+      <img src={logo} />
       <div>{description}</div>
       <h3>
         <Link to={`/portfolio/${id}`}>link</Link>
