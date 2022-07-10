ba8bd7ee30266f56a9862f80023613cd6f357c86
alejandroserrano <u1214728@utah.edu>
2022-07-10 14:11:55 -0600

    this is little progress but something

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function (props) {
-  const { id, description, thumb_image_url, logo } = props.item;
-  return (
-    <div className="portfolio-item-wrapper">
-      <div className="portfolio-img-background" 
-      style={{backgroundImage: "url(" + thumb_image_url + ")"}}/>
-      <img src={logo} />
-      <div>{description}</div>
-      <h3>
-        <Link to={`/portfolio/${id}`}>link</Link>
-      </h3>
-    </div>
-  );
+export default class PortfolioItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      portfolioItemClass: "",
+    };
+  }
+
+  handleMouseEnter() {
+    this.setState({ portfolioItemClass: "image-blur" });
+  }
+
+  handleMouseLeave() {
+    this.setState({
+      portfolioItemClass: "",
+    });
+  }
+
+  render() {
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
+    return (
+      <div
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
+      <h1>{this.state.portfolioItemClass}</h1>
+        <div
+          className={"portfolio-img-background " + this.state.portfolioItemClass}
+          style={{ backgroundImage: "url(" + thumb_image_url + ")" }}
+        />
+
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} />
+          </div>
+          <div className="subtitle">{description}</div>
+        </div>
+      </div>
+    );
+  }
 }
